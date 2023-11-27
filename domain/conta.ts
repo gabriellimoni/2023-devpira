2e07ef74f63dc216ab74491da8bae8af3f45b0e2
Gabriel Limoni <gabriel.limoni@liven.tech>
2023-11-27 17:55:00 -0300

    adicionando conta corrente e suas funções principais que serão exemplificadas

---
+++ domain/conta.ts
@@ -0,0 +1,27 @@
+export interface Conta {
+  id: string;
+  agencia: string;
+  numero: string;
+  senha: string;
+  saldo: number;
+}
+
+export function realizarSaque(
+  conta: Conta,
+  senha: string,
+  valor: number
+): Conta {
+  return {} as Conta;
+}
+
+export function realizarDeposito(conta: Conta, valor: number): Conta {
+  return {} as Conta;
+}
+
+export function contratarChequeEspecial(
+  conta: Conta,
+  senha: string,
+  valor: number
+): Conta {
+  return {} as Conta;
+}
